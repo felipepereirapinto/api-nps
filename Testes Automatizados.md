24338b5cd8c316ae9bebef32f0f168ba862aab74
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2021-02-25 02:48:46 -0300

    automated tests - works half the time

---
+++ Testes Automatizados.md
@@ -0,0 +1,18 @@
+# Testes Automatizados
+
+## 1 - Testes unitários
+
+Dados Fake
+
+## 2 - Testes de integração
+
+Usado na NLW#04
+
+-> request -> routes -> controller -> repository
+<- repository <- controller <- response
+
+Apenas dentro da aplicação
+
+## 3 - Ponta a Ponta (E2E)
+
+Leva em conta o frontend tbm
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,29 +1,29 @@
-import { Request, response, Response } from 'express'
-import { getRepository } from 'typeorm'
-import { User } from '../models/User'
+import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
 
 class UserController {
   async create(req: Request, res: Response) {
     const { name, email } = req.body
 
-    const usersRepository = getRepository(User)
+    const usersRepository = getCustomRepository(UsersRepository)
 
-    const userAlreadyExists = await usersRepository.findOne({ email, })
+    const userAlreadyExists = await usersRepository.findOne({ email })
 
     if (userAlreadyExists) {
       return res.status(400).json({
-        error: "User already exists!",
+        error: "User already exists!"
       })
     }
 
     const user = usersRepository.create({
       name,
-      email,
+      email
     })
 
     await usersRepository.save(user)
 
-    return res.json(user)
+    return res.status(201).json(user)
   }
   
 }
